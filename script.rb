2c25cd9557e7fe7dfcf86f9aba74d8668f6d5f88
yassine.aouadi <yassine.aouadi@hotmail.fr>
2016-04-16 16:39:58 +0100

    connexion a redis ,  parsing et  insertion du contenu du fichier log dans la base

--- script.rb
+++ script.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
 
-
-
 #on ajoute les librairies pour json
 require 'rubygems'
+system 'gem install json > log.txt && cat log.txt'
 require 'json'
 
 system 'echo "updating..."'
-#system 'apt-get update'
+system 'apt-get update -y >> log.txt && cat log.txt  '
 #lire le fichier json
 json = File.read('config.json')
 obj = JSON.parse(json)
@@ -33,18 +32,46 @@ cibleDns.write("nameserver ".concat($nameserver.to_s))
 puts'fin de la confiiguration du serveur ubuntu'
 
 puts 'installation de nginx'
-system "apt-get install -y nginx >  /dev/null 2>&1"
-system "update-rc.d  nginx enable > /dev/null 2>&1 "
-system "service nginx restart > /dev/null 2>&1"
-system "service nginx status"
+system "apt-get install -y nginx >>  log.txt && cat log.txt"
+system "update-rc.d  nginx enable >>  log.txt && cat  log.txt "
+system "service nginx restart >> log.txt && cat log.txt"
+system "service nginx status "
 
 puts 'installation de redis server'
-system  "apt-get -y install -y redis-server > /dev/null 2>&1"
+system  "apt-get -y install -y redis-server >> log.txt && cat log.txt"
 puts 'status du service redis-server'
-system " service redis-server status"
+system " service redis-server restart  >> log.txt  && cat log.txt"
 puts  'lancement du serveur '
 system " service redis-server restart"
 puts 'fin du deploiement de nginx et redis'
+
+#installation du module redis pour ruby
+system 'gem install redis >> log.txt  && cat log.txt'
+system 'apt-get install bundler >> log.txt && cat log.txt  '
+
+puts 'insertion du log dans la base de redis'
+
+#parsing du fichier log du script 
+fichier = File.open("log.txt", "r")
+chaine=""
+
+fichier.each_line { |ligne|
+ chaine= chaine.concat"#{ligne}"
+}
+fichier.close
+
+#ouvrir une connexion  et insertion dans la base de donée
+
+require 'redis'
+#ouvrir une connexion
+redis=Redis.new(:host => 'localhost', :port => 6379)
+#insertion dans la base du contenu du fichier log 
+redis.set('log', chaine)
+value = redis.get('log');
+#verification de insertion
+puts value
+
+
 
 #Generation du fichier html
 puts 'generation du fichier html'
@@ -67,4 +94,6 @@ cibleHtml.write(" <!DOCTYPE html>
 </body>
 </html>
 ")
-system "service nginx restart" 
+puts 'redemarrer nginx pour prendre en compte les changement'
+system "service nginx restart"
+
